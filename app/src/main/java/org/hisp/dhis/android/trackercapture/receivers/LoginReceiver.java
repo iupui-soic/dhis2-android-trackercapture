700a7b5e1df12ab766b41f58a0a62ccac3697d75
Saptarshi Purkayastha <sunbiz@gmail.com>
2018-08-13 21:47:11 -0400

    Sending valid Pins from tracker-capture

--- app/src/main/java/org/hisp/dhis/android/trackercapture/receivers/LoginReceiver.java
+++ app/src/main/java/org/hisp/dhis/android/trackercapture/receivers/LoginReceiver.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 
 import org.hisp.dhis.android.sdk.controllers.DhisController;
 import org.hisp.dhis.android.sdk.controllers.metadata.MetaDataController;
+import org.hisp.dhis.android.sdk.controllers.tracker.TrackerController;
+import org.hisp.dhis.android.sdk.persistence.models.Enrollment;
 
 import java.util.ArrayList;
 
@@ -18,6 +20,7 @@ import java.util.ArrayList;
 public class LoginReceiver extends BroadcastReceiver {
     private static final String APPS_MHBS_TRAINING_PACKAGE = "edu.iupui.soic.biohealth.plhi.mhbs";
     private static final String keyRequest = "key:loginRequest";
+    private static final String PIN_ID = "Nf2mWKVwmGK";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -26,9 +29,17 @@ public class LoginReceiver extends BroadcastReceiver {
 
             // get user credentials
             ArrayList<String> userDetails = new ArrayList<>();
+            StringBuffer validPins = new StringBuffer();
             userDetails.add(0, DhisController.getInstance().getSession().getCredentials().getUsername());
             userDetails.add(1, DhisController.getInstance().getSession().getCredentials().getPassword());
             userDetails.add(2, DhisController.getInstance().getSession().getServerUrl().toString());
+            for (Enrollment enrollment : TrackerController.getActiveEnrollments()) {
+                if (enrollment.getAttributes().get(4).getTrackedEntityAttributeId().equals(PIN_ID)) {
+                    validPins.append(enrollment.getAttributes().get(4).getValue() + ",");
+                }
+            }
+            userDetails.add(3, validPins.toString());
+
             // use explicit front door intent to launch the login on training app with the user credentials
             Intent i = context.getPackageManager().getLaunchIntentForPackage(APPS_MHBS_TRAINING_PACKAGE);
             i.putExtra(keyRequest, userDetails);
